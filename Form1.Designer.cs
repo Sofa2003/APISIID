a266912403a6d43f1a38bb88a92a953fdb1d3f37
sofa b <sborovkova168@gmail.com>
2024-06-07 02:56:18 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace APISIID
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonCreate = new Button();
+            SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            buttonCreate.Location = new Point(399, 210);
+            buttonCreate.Name = "buttonCreate";
+            buttonCreate.Size = new Size(94, 29);
+            buttonCreate.TabIndex = 0;
+            buttonCreate.Text = "button1";
+            buttonCreate.UseVisualStyleBackColor = true;
+            buttonCreate.Click += buttonCreate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonCreate);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonCreate;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+using Stars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Windows.Forms;
+using TFlex;
+using TFlex.Configuration;
+using TFlex.Model;
+using TFlex.Model.Model2D;
+
+
+namespace APISIID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        public void buttonCreate_Click(object sender, EventArgs e)
+        {
+            StarPropertiesControls propertiesControls =  new StarPropertiesControls();
+
+            
+
+        }
+    }
+}
+
+    
+
+
